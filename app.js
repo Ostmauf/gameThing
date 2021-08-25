c682ac0eb0b2fe33fd082cb4fcfb5e8e11f6b348
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-08-25 14:02:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let keys = [];
 canvas.width = 800;
 canvas.height = 600;
 groundHeight = -50;
+player1Speed = 1;
 
 
 class Box {
@@ -21,8 +22,9 @@ class Box {
     }
 }
 //Skapar objekt till spelet
-player1 = new Box(10, 550, 50, 50, 'blue')
-ground = new Box(0, canvas.height, canvas.width, groundHeight, "black")
+player1 = new Box(10, 550, 50, 50, 'blue');
+ground = new Box(0, canvas.height, canvas.width, groundHeight, "lightgreen");
+platform = new Box(300, 350, 60, 30, "purple");
 
 //Skapar spelets loop
 setInterval(gameLoop, 1)
@@ -38,6 +40,7 @@ window.addEventListener("keyup", (e) => {
 function gameLoop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ground.draw()
+    platform.draw()
     player1.draw()
     move()
     checkWalls()
@@ -46,25 +49,31 @@ function gameLoop(){
 function move(){
     if (player1.y > 0) {
         if (keys[87]) {
-            player1.y -= 1;
+            player1.y -= player1Speed;
         }
     }
-    if (player1.y < (canvas.height - 50)) {
+    if (player1.y + player1.h < canvas.height + groundHeight) {
         if (keys[83]) {
-            player1.y += 1;
+            player1.y += player1Speed;
         }
     }
     if (player1.x < (canvas.width - 50)) {
         if (keys[68]) {
-            player1.x += 1;
+            player1.x += player1Speed;
         }
     }
    if (player1.x > 0) {
         if (keys[65]) {
-            player1.x -= 1;
+            player1.x -= player1Speed;
         }
     }
+    if (player1.y < platform.y || player1.x > (platform.x - 50)) {
+        console.log("HIT!!!")
+    }
+
 }
+
+
 
 function checkWalls(){
 
